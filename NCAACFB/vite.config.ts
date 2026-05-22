cffcd95f9b0a29a41d69c73e6420fe45bc0212b3
dstaggs-15 <daniel.staggs0215@gmail.com>
2026-05-22 09:41:58 -0500

    Update vite.config.ts

--- NCAACFB/vite.config.ts
+++ NCAACFB/vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
         dynasty: resolve(__dirname, 'dynasty/index.html'),
         dynastyAdd: resolve(__dirname, 'dynasty/add/index.html'),
         dynastySeasons: resolve(__dirname, 'dynasty/seasons/index.html'),
+        dynastyCoaches: resolve(__dirname, 'dynasty/coaches/index.html'),
+        dynastyStandings: resolve(__dirname, 'dynasty/standings/index.html'),
         blogger: resolve(__dirname, 'blogger/index.html'),
       }
     }
