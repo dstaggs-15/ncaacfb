edb83960645ac7b909cdc49d24f4aa916dcec80a
dstaggs-15 <daniel.staggs0215@gmail.com>
2026-05-22 08:49:14 -0500

    Add dynastySeasons entry to Vite config

--- NCAACFB/vite.config.ts
+++ NCAACFB/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         main: 'index.html',
         dynasty: 'dynasty/index.html',
         dynastyAdd: 'dynasty/add/index.html',
+        dynastySeasons: 'dynasty/seasons/index.html',
         blogger: 'blogger/index.html',
       }
     }
